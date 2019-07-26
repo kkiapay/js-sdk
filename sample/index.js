e98845d89fb4fd50785b82202ede172f14128c30
Boussari Latif <latif.boussari@mehriso.com>
2019-07-26 11:05:09 +0100

    add direct environnement

--- sample/index.js
+++ sample/index.js
@@ -1,9 +1,8 @@
 const kkiapay = require('../index')(
-    "7b1133f07cc411e99c4c5372e18c065c",{ Test:true }
+    "XXXXXXXXXXXXX",{ Test:true }
 )
 
 
-22997000000
 
 kkiapay.debitRequest("22997000000",1,"JOHN","DOE")
 .then((response)=>{
