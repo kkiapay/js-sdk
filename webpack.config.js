ce3f356e80232df40441f3828efa174acec5ca0a
Boussari Latif <latif.boussari@mehriso.com>
2020-04-04 18:11:07 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,18 @@
 
 
 const path = require("path");
-const webpack = require("webpack");
 const webpack_rules = [];
-
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const babelLoader = {
     test: /\.js$/,
-    // exclude: /(node_modules|bower_components)/,
     use: {
         loader: "babel-loader",
         options: {
             "presets": [
                 [
-                    "@babel/preset-env", {
-                //   "modules": "commonjs"
-                }
-            ],
+                    "@babel/preset-env"
+                ],
               ],
         }
     }
